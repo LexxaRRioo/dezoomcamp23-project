af1ad634aaa9342776487451465c0d3976f962fd
Alex Razvodov <razvodov.alexey@gmail.com>
2023-04-24 18:46:31 +0000

    fixed tables and created views in ch based on ad-hoc queries
    next - transfer it to dbt for data lineage and other features

--- dez_dbt/clickhouse.sql
+++ dez_dbt/clickhouse.sql
@@ -1,69 +1,127 @@
-drop table dezdb.games_detailed_info;
+drop table if exists dezdb.stg_games_detailed;
 
-create table dezdb.games_detailed_info (
-     rowid UInt32
-    ,type String
-    ,id UInt64
-    ,thumbnail String
-    ,image String
-    ,primary String
-    ,alternate String
-    ,description String
-    ,yearpublished UInt16
-    ,minplayers UInt8
-    ,maxplayers UInt8
-    ,suggested_num_players String
-    ,suggested_playerage String
-    ,suggested_language_dependence String
-    ,playingtime UInt8
-    ,minplaytime UInt8
-    ,maxplaytime UInt8
-    ,minage UInt8
-    ,boardgamecategory String
-    ,boardgamemechanic String
-    ,boardgamefamily String
-    ,boardgameexpansion String
-    ,boardgameimplementation String
-    ,boardgamedesigner String
-    ,boardgameartist String
-    ,boardgamepublisher String
-    ,usersrated UInt32
-    ,average Float32
-    ,bayesaverage Float32
-    ,Board_Game_Rank UInt32
-    ,Strategy_Game_Rank UInt32
-    ,Family_Game_Rank UInt32
-    ,stddev Float32
-    ,median Float32
-    ,owned UInt32
-    ,trading UInt32
-    ,wanting UInt32
-    ,wishing UInt32
-    ,numcomments UInt32
-    ,numweights UInt32
-    ,averageweight Float32
-    ,boardgameintegration String
-    ,boardgamecompilation String
-    ,Party_Game_Rank UInt32
-    ,Abstract_Game_Rank UInt32
-    ,Thematic_Rank UInt32
-    ,War_Game_Rank UInt32
-    ,Customizable_Rank UInt32
-    ,Childrens_Game_Rank UInt32
-    ,RPG_Item_Rank UInt32
-    ,Accessory_Rank UInt32
-    ,Video_Game_Rank UInt32
-    ,Amiga_Rank UInt32
-    ,Commodore_64_Rank UInt32
-    ,Arcade_Rank UInt32
-    ,Atari_ST_Rank UInt32
-) ENGINE = MergeTree 
---partition by yearpublished
-order by id;
+drop table if exists dezdb.stg_games_detailed;
 
-INSERT INTO dezdb.games_detailed_info
-SELECT rowid
-,type
+create table if not exists dezdb.stg_games_detailed
+engine MergeTree
+order by id
+as select type
+,ID as id
+,thumbnail
+,image
+,primary
+,alternate
+,description
+,yearpublished
+,minplayers
+,maxplayers
+,suggested_num_players
+,suggested_playerage
+,suggested_language_dependence
+,playingtime
+,minplaytime
+,maxplaytime
+,minage
+,boardgamecategory
+,boardgamemechanic
+,boardgamefamily
+,boardgameexpansion
+,boardgameimplementation
+,boardgamedesigner
+,boardgameartist
+,boardgamepublisher
+,usersrated
+,average
+,bayesaverage
+,"Board Game Rank"
+,"Strategy Game Rank"
+,"Family Game Rank"
+,stddev
+,median
+,owned
+,trading
+,wanting
+,wishing
+,numcomments
+,numweights
+,averageweight
+,boardgameintegration
+,boardgamecompilation
+,"Party Game Rank"
+,"Abstract Game Rank"
+,"Thematic Rank"
+,"War Game Rank"
+,"Customizable Rank"
+,"Children's Game Rank"
+,"RPG Item Rank"
+,"Accessory Rank"
+,"Video Game Rank"
+,"Amiga Rank"
+,"Commodore 64 Rank"
+,"Arcade Rank"
+,"Atari ST Rank"
+FROM s3('https://storage.yandexcloud.net/dez-rzv-final-project/data/games_detailed_info.csv', 'CSVWithNames', 'type String,ID Int32,thumbnail String,image String,primary String,alternate String,description String,yearpublished Int16,minplayers Int8,maxplayers Int8,suggested_num_players String,suggested_playerage String,suggested_language_dependence String,playingtime Int8,minplaytime Int8,maxplaytime Int8,minage Int8,boardgamecategory String,boardgamemechanic String,boardgamefamily String,boardgameexpansion String,boardgameimplementation String,boardgamedesigner String,boardgameartist String,boardgamepublisher String,usersrated Int32,average decimal(10,7),bayesaverage decimal(10,7),"Board Game Rank" String,"Strategy Game Rank" String,"Family Game Rank" String,stddev decimal(10,7),median decimal(10,7),owned Int32,trading Int32,wanting Int32,wishing Int32,numcomments Int32,numweights Int32,averageweight decimal(10,7),boardgameintegration String,boardgamecompilation String,"Party Game Rank" String,"Abstract Game Rank" String,"Thematic Rank" String,"War Game Rank" String,"Customizable Rank" String,"Children''s Game Rank" String,"RPG Item Rank" String,"Accessory Rank" String,"Video Game Rank" String,"Amiga Rank" String,"Commodore 64 Rank" String,"Arcade Rank" String,"Atari ST Rank" String');
+
+
+drop table if exists dezdb.stg_games_aggregated;
+
+create table if not exists dezdb.stg_games_aggregated
+engine MergeTree
+order by id
+as select ID as id
+,Name
+,Year
+,Rank
+,Average
+,"Bayes average"
+,"Users rated"
+,URL
+,Thumbnail
+FROM s3('https://storage.yandexcloud.net/dez-rzv-final-project/data/2022-01-08.csv',  'CSVWithNames', 'ID UInt32,Name String,Year UInt16,Rank UInt16,Average decimal(10,5),"Bayes average" decimal(10,5),"Users rated" UInt32,URL String,Thumbnail String');
+
+drop table if exists dezdb.stg_games_reviews;
+
+create table if not exists dezdb.stg_games_reviews
+engine MergeTree
+order by id
+as select user
+,rating
+,comment
+,ID as id
+,name
+FROM s3('https://storage.yandexcloud.net/dez-rzv-final-project/data/bgg-19m-reviews.csv', 'CSVWithNames', 'user String,rating decimal(10,5),comment String,ID UInt32,name String');
+
+
+select * from dezdb.games_aggregated
+limit 10;
+
+select * from dezdb.games_detailed
+limit 10;
+
+select * from dezdb.games_reviews
+limit 10;
+
+
+create or replace view dezdb.v_reviews
+as 
+select r.user
+,r.rating
+,case when empty(r.comment) then null else r.comment end comment
+,r.id
+,r.name
+,case 
+    when length(r.comment) <= 0 then 'Empty'
+    when length(r.comment) between 1 and 25 then '1 to 25 chars'
+    when length(r.comment) between 26 and 50 then '26 to 50 chars'
+    when length(r.comment) between 51 and 100 then '51 to 100 chars'
+    when length(r.comment) between 100 and 250 then '100 to 250 chars'
+    when length(r.comment) > 250 then 'More than 250 chars'
+    end comment_len_bucket
+from dezdb.games_reviews r;
+
+create or replace view dezdb.v_detailed
+as 
+select type
 ,id
 ,thumbnail
 ,image
@@ -91,9 +149,9 @@ SELECT rowid
 ,usersrated
 ,average
 ,bayesaverage
-,"Board Game Rank" as Board_Game_Rank
-,"Strategy Game Rank" as Strategy_Game_Rank
-,"Family Game Rank" as Family_Game_Rank
+,toUInt32OrNull("Board Game Rank") "Board Game Rank"
+,toUInt32OrNull("Strategy Game Rank") "Strategy Game Rank"
+,toUInt32OrNull("Family Game Rank") "Family Game Rank"
 ,stddev
 ,median
 ,owned
@@ -105,32 +163,64 @@ SELECT rowid
 ,averageweight
 ,boardgameintegration
 ,boardgamecompilation
-,"Party Game Rank" as Party_Game_Rank
-,"Abstract Game Rank" as Abstract_Game_Rank
-,"Thematic Rank" as Thematic_Rank
-,"War Game Rank" as War_Game_Rank
-,"Customizable Rank" as Customizable_Rank
-,"Children's Game Rank" as Childrens_Game_Rank
-,"RPG Item Rank" as RPG_Item_Rank
-,"Accessory Rank" as Accessory_Rank
-,"Video Game Rank" as Video_Game_Rank
-,"Amiga Rank" as Amiga_Rank
-,"Commodore 64 Rank" as Commodore_64_Rank
-,"Arcade Rank" as Arcade_Rank
-,"Atari ST Rank" as Atari_ST_Rank
-FROM s3('https://storage.yandexcloud.net/dez-rzv-final-project/data/games_detailed_info.csv','','',  'CSVWithNames');
+,toUInt32OrNull("Party Game Rank") "Party Game Rank"
+,toUInt32OrNull("Abstract Game Rank") "Abstract Game Rank"
+,toUInt32OrNull("Thematic Rank") "Thematic Rank"
+,toUInt32OrNull("War Game Rank") "War Game Rank"
+,toUInt32OrNull("Customizable Rank") "Customizable Rank"
+,toUInt32OrNull("Children's Game Rank") "Children's Game Rank"
+,toUInt32OrNull("RPG Item Rank") "RPG Item Rank"
+,toUInt32OrNull("Accessory Rank") "Accessory Rank"
+,toUInt32OrNull("Video Game Rank") "Video Game Rank"
+,toUInt32OrNull("Amiga Rank") "Amiga Rank"
+,toUInt32OrNull("Commodore 64 Rank") "Commodore 64 Rank"
+,toUInt32OrNull("Arcade Rank") "Arcade Rank"
+,toUInt32OrNull("Atari ST Rank") "Atari ST Rank"
+from dezdb.games_detailed;
 
+create or replace view dezdb.v_reviews_agg_by_comm_bucket
+as 
+select r.comment_len_bucket
+,round(avg(rating),3) as avg_rating
+,count(rating) as cnt
+from dezdb.v_reviews r
+group by r.comment_len_bucket;
 
-create table dezdb.games_aggregated_data
-engine MergeTree
-order by id
-as select ID as id
-,Name
-,Year
-,Rank
-,Average
-,"Bayes average"
-,"Users rated"
-,URL
-,Thumbnail
-FROM s3('https://storage.yandexcloud.net/dez-rzv-final-project/data/2022-01-08.csv','','',  'CSVWithNames', 'ID UInt32,Name String,Year UInt16,Rank UInt16,Average decimal(18,6),"Bayes average" decimal(18,6),"Users rated" UInt32,URL String,Thumbnail String');
+create or replace view dezdb.v_ratings_agg_by_year
+as select Year
+, max("Bayes average") as mx_avg
+, min("Bayes average") as mn_avg
+, max("Users rated") as mx_usrs_rated
+, min("Users rated") as mn_usrs_rated
+, max(Rank) as mx_rank
+, min(Rank) as mn_rank
+, count(1) as cnt_published
+from dezdb.games_aggregated
+where Year > 0
+group by Year;
+
+select 
+    case 
+        when Year < 1 then 'bc'
+        when Year between 1 and 1000 then '1 to 1000'
+        when Year between 1001 and 1500 then '1001 to 1500'
+        when Year between 1501 and 1750 then '1501 to 1750'
+        when Year between 1751 and 1900 then '1751 to 1900'
+        when Year between 1901 and 1950 then '1901 to 1950'
+        when Year between 1951 and 1975 then '1951 to 1975'
+        when Year between 1976 and 1990 then '1976 to 1990'
+        when Year between 1991 and 2000 then '1991 to 2000'
+        when Year between 2001 and 2010 then '2001 to 2010'
+        when Year between 2011 and 2020 then '2011 to 2020'
+        when Year between 2021 and toInt32(date_trunc('year', now())) then '2021 to now'
+        when Year >= toInt32(date_trunc('year', now())) then 'Future'
+    end year_bucket
+    , max("Bayes average") as mx_avg
+    , min("Bayes average") as mn_avg
+    , max("Users rated") as mx_usrs_rated
+    , min("Users rated") as mn_usrs_rated
+    , max(Rank) as mx_rank
+    , min(Rank) as mn_rank
+    , count(1) as cnt_published
+from dezdb.games_aggregated
+group by year_bucket;
